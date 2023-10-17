a77310c6e692d3505c4bdcc83973561e82c352a6
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2023-10-17 09:35:33 +0200

    serve peublic folder

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
 app.use(express.static('public/voice'));
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'public', 'voice')))
 
 const AWS = require("aws-sdk");
